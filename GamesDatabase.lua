4c68ab1eeabcb6571a0aab4938aef42732b7b4cf
Mikazu <gilangirchas15@gmail.com>
2026-05-21 20:35:17 +0700

    Add GamesDatabase.lua with game scripts

---
+++ GamesDatabase.lua
@@ -0,0 +1,19 @@
+return {
+    -- RELAPSE Seamin Tak Seiman
+    [108187800843065] = {
+        Name = "RELAPSE Seamin Tak Seiman", 
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/Relapse.lua"
+    },
+    
+    -- Balapan Menara Matematika
+    [17571821642] = {
+        Name = "Balapan Menara Matematika", 
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/BalapanMenaraMatematika.lua"
+    },
+    
+    -- Sore✨[Ava Gratis + Duyung]
+    [111208180846561] = {
+        Name = "Sore✨[Ava Gratis + Duyung]", 
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/SoreAvaGratisDuyung.lua"
+    }
+}
