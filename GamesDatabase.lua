4fe90dd96a454545e7b65e46d08af537516111d4
Mikazu <gilangirchas15@gmail.com>
2026-05-25 16:12:19 +0700

    Update GamesDatabase.lua

--- GamesDatabase.lua
+++ GamesDatabase.lua
@@ -1,5 +1,5 @@
 --========================================================================
--- [MODUL 3] DATABASE PERUTEAN OTOMATIS
+-- [MODUL 3] DATABASE PERUTEAN OTOMATIS (MIZUKAGE CLOUD HUB)
 --========================================================================
 return {
     -- RELAPSE Seamin Tak Seiman
@@ -30,5 +30,109 @@ return {
     [131848958487439] = {
         Name = "KARTA SUNDA - Ruang Riung", 
         Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/KartaSundaRuangRiung.lua"
+    },
+
+    -- Sawit Garden
+    [GANTI_DENGAN_PLACE_ID_SAWIT_GARDEN] = {
+        Name = "Sawit Garden",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/SawitGarden.lua"
+    },
+
+    -- Sawah Indo
+    [GANTI_DENGAN_PLACE_ID_SAWAH_INDO] = {
+        Name = "Sawah Indo",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/SawahIndo.lua"
+    },
+
+    -- Sambung Kata
+    [GANTI_DENGAN_PLACE_ID_SAMBUNG_KATA] = {
+        Name = "Sambung Kata",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/SambungKata.lua"
+    },
+
+    -- Blade Ball
+    [GANTI_DENGAN_PLACE_ID_BLADE_BALL] = {
+        Name = "Blade Ball",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/BladeBall.lua"
+    },
+
+    -- Aura Trade
+    [GANTI_DENGAN_PLACE_ID_AURA_TRADE] = {
+        Name = "Aura Trade",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/AuraTrade.lua"
+    },
+
+    -- Brookhaven
+    [GANTI_DENGAN_PLACE_ID_BROOKHAVEN] = {
+        Name = "Brookhaven",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/Brookhaven.lua"
+    },
+
+    -- Demonology
+    [GANTI_DENGAN_PLACE_ID_DEMONOLOGY] = {
+        Name = "Demonology",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/Demonology.lua"
+    },
+
+    -- Evade
+    [GANTI_DENGAN_PLACE_ID_EVADE] = {
+        Name = "Evade",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/Evade.lua"
+    },
+
+    -- Flick
+    [GANTI_DENGAN_PLACE_ID_FLICK] = {
+        Name = "Flick",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/Flick.lua"
+    },
+
+    -- Minesweeper
+    [GANTI_DENGAN_PLACE_ID_MINESWEEPER] = {
+        Name = "Minesweeper",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/Minesweeper.lua"
+    },
+
+    -- Murder Mystery 2
+    [GANTI_DENGAN_PLACE_ID_MM2] = {
+        Name = "Murder Mystery 2",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/MM2.lua"
+    },
+
+    -- Poop Game
+    [GANTI_DENGAN_PLACE_ID_POOP_GAME] = {
+        Name = "Poop Game",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/PoopGame.lua"
+    },
+
+    -- Indo Beach
+    [GANTI_DENGAN_PLACE_ID_INDO_BEACH] = {
+        Name = "Indo Beach",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/IndoBeach.lua"
+    },
+
+    -- Blox Fruits
+    [2753915549] = {
+        Name = "Blox Fruits - World 1",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/BloxFruits.lua"
+    },
+    [4442272183] = {
+        Name = "Blox Fruits - World 2",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/BloxFruits.lua"
+    },
+    [7449423635] = {
+        Name = "Blox Fruits - World 3",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/BloxFruits.lua"
+    },
+
+    -- Violence District
+    [GANTI_DENGAN_PLACE_ID_VD] = {
+        Name = "Violence District",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/ViolenceDistrict.lua"
+    },
+
+    -- Tebak Yuk
+    [GANTI_DENGAN_PLACE_ID_TEBAK_YUK] = {
+        Name = "Tebak Yuk",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/TebakYuk.lua"
     }
 }
