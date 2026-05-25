b83b2fd84196d5b16d967be38fa6f524291a3351
Mikazu <gilangirchas15@gmail.com>
2026-05-25 16:15:05 +0700

    Update GamesDatabase.lua

--- GamesDatabase.lua
+++ GamesDatabase.lua
@@ -2,52 +2,16 @@
 -- [MODUL 3] DATABASE PERUTEAN OTOMATIS (MIZUKAGE CLOUD HUB)
 --========================================================================
 return {
-    -- RELAPSE Seamin Tak Seiman
-    [108187800843065] = {
-        Name = "RELAPSE Seamin Tak Seiman", 
-        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/Relapse.lua"
+    -- Aura Trade
+    [GANTI_DENGAN_PLACE_ID_AURA_TRADE] = {
+        Name = "Aura Trade",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/AuraTrade.lua"
     },
-    
+
     -- Balapan Menara Matematika
     [17571821642] = {
         Name = "Balapan Menara Matematika", 
         Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/BalapanMenaraMatematika.lua"
-    },
-    
-    -- Sore✨[Ava Gratis + Duyung]
-    [111208180846561] = {
-        Name = "Sore✨[Ava Gratis + Duyung]", 
-        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/SoreAvaGratisDuyung.lua"
-    },
-
-    -- Jiwa Besi: Dungeon / Hutan Tanpa Bintang
-    [114831977475110] = {
-        Name = "Jiwa Besi: Dungeon / Hutan Tanpa Bintang", 
-        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/JiwaBesiDungeon.lua"
-    },
-
-    -- KARTA SUNDA - Ruang Riung
-    [131848958487439] = {
-        Name = "KARTA SUNDA - Ruang Riung", 
-        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/KartaSundaRuangRiung.lua"
-    },
-
-    -- Sawit Garden
-    [GANTI_DENGAN_PLACE_ID_SAWIT_GARDEN] = {
-        Name = "Sawit Garden",
-        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/SawitGarden.lua"
-    },
-
-    -- Sawah Indo
-    [GANTI_DENGAN_PLACE_ID_SAWAH_INDO] = {
-        Name = "Sawah Indo",
-        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/SawahIndo.lua"
-    },
-
-    -- Sambung Kata
-    [GANTI_DENGAN_PLACE_ID_SAMBUNG_KATA] = {
-        Name = "Sambung Kata",
-        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/SambungKata.lua"
     },
 
     -- Blade Ball
@@ -56,10 +20,18 @@ return {
         Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/BladeBall.lua"
     },
 
-    -- Aura Trade
-    [GANTI_DENGAN_PLACE_ID_AURA_TRADE] = {
-        Name = "Aura Trade",
-        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/AuraTrade.lua"
+    -- Blox Fruits
+    [2753915549] = {
+        Name = "Blox Fruits - World 1",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/BloxFruits.lua"
+    },
+    [4442272183] = {
+        Name = "Blox Fruits - World 2",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/BloxFruits.lua"
+    },
+    [7449423635] = {
+        Name = "Blox Fruits - World 3",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/BloxFruits.lua"
     },
 
     -- Brookhaven
@@ -86,6 +58,24 @@ return {
         Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/Flick.lua"
     },
 
+    -- Indo Beach
+    [GANTI_DENGAN_PLACE_ID_INDO_BEACH] = {
+        Name = "Indo Beach",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/IndoBeach.lua"
+    },
+
+    -- Jiwa Besi: Dungeon / Hutan Tanpa Bintang
+    [114831977475110] = {
+        Name = "Jiwa Besi: Dungeon / Hutan Tanpa Bintang", 
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/JiwaBesiDungeon.lua"
+    },
+
+    -- KARTA SUNDA - Ruang Riung
+    [131848958487439] = {
+        Name = "KARTA SUNDA - Ruang Riung", 
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/KartaSundaRuangRiung.lua"
+    },
+
     -- Minesweeper
     [GANTI_DENGAN_PLACE_ID_MINESWEEPER] = {
         Name = "Minesweeper",
@@ -104,35 +94,45 @@ return {
         Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/PoopGame.lua"
     },
 
-    -- Indo Beach
-    [GANTI_DENGAN_PLACE_ID_INDO_BEACH] = {
-        Name = "Indo Beach",
-        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/IndoBeach.lua"
+    -- RELAPSE Seamin Tak Seiman
+    [108187800843065] = {
+        Name = "RELAPSE Seamin Tak Seiman", 
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/Relapse.lua"
     },
 
-    -- Blox Fruits
-    [2753915549] = {
-        Name = "Blox Fruits - World 1",
-        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/BloxFruits.lua"
-    },
-    [4442272183] = {
-        Name = "Blox Fruits - World 2",
-        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/BloxFruits.lua"
-    },
-    [7449423635] = {
-        Name = "Blox Fruits - World 3",
-        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/BloxFruits.lua"
+    -- Sambung Kata
+    [GANTI_DENGAN_PLACE_ID_SAMBUNG_KATA] = {
+        Name = "Sambung Kata",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/SambungKata.lua"
     },
 
-    -- Violence District
-    [GANTI_DENGAN_PLACE_ID_VD] = {
-        Name = "Violence District",
-        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/ViolenceDistrict.lua"
+    -- Sawah Indo
+    [GANTI_DENGAN_PLACE_ID_SAWAH_INDO] = {
+        Name = "Sawah Indo",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/SawahIndo.lua"
+    },
+
+    -- Sawit Garden
+    [GANTI_DENGAN_PLACE_ID_SAWIT_GARDEN] = {
+        Name = "Sawit Garden",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/SawitGarden.lua"
+    },
+
+    -- Sore✨[Ava Gratis + Duyung]
+    [111208180846561] = {
+        Name = "Sore✨[Ava Gratis + Duyung]", 
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/SoreAvaGratisDuyung.lua"
     },
 
     -- Tebak Yuk
     [GANTI_DENGAN_PLACE_ID_TEBAK_YUK] = {
         Name = "Tebak Yuk",
         Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/TebakYuk.lua"
+    },
+
+    -- Violence District
+    [GANTI_DENGAN_PLACE_ID_VD] = {
+        Name = "Violence District",
+        Script = "https://raw.githubusercontent.com/MikazuDbug/TeamMizu-HUB/refs/heads/main/Local%20script/ViolenceDistrict.lua"
     }
 }
